33f65394c2a1e6ea0c2355bd5c2ebbfcfaf825e0
Your Name <you@example.com>
2020-12-02 23:14:29 +0530

    count with condition example

--- vartest/vartest.tf
+++ vartest/vartest.tf
@@ -1,10 +1,11 @@
 provider "aws" {
-    region = var.region
+    region = "ap-northeast-2"
     
+     
 }
 
-
 # count examples it will create 3 users with like user1 user2 user3
+/*
 resource "aws_iam_user" "iamuser" {
     #name = "user${count.index}"
     name = var.iamusers[count.index]
@@ -12,9 +13,28 @@ resource "aws_iam_user" "iamuser" {
     count = 3
   
 }
+*/
 variable "iamusers" {
     type = list
     default = ["naveen","lucky","nikhil"]
   
 }
+
+# count with with condition
+variable "istest" {
+    default=false
+}
+resource "aws_iam_user" "true" {
+    name = "naveen1"
+    path = "/system"
+    count = var.istest == true ? 1 : 0
+}
+resource "aws_iam_user" "false" {
+    name = var.iamusers[count.index]
+    path = "/system"
+    count = var.istest == false ? 3 : 0
+  
+}
+
+
 
